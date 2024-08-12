76309ecfecc8f9f7dfc70846d60dcb6fb549c723
unknown <pjuned.it@gmail.com>
2024-08-12 17:50:54 +0530

    second script

--- datatypes.sh
+++ datatypes.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 echo enter 2 numbers to get their sum
- int number1=$1
- int number2=$2
+ number1=$1
+  number2=$2
 
- int sum=$(($number1)+($number2))
+ sum=$(($number1)+($number2))
 
  echi "sum is $(sum)"
