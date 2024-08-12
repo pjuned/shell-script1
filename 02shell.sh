3b7b0a5c3271a428cb4c4dd5fa946cc62778001a
unknown <pjuned.it@gmail.com>
2024-08-12 17:14:25 +0530

    second script

--- 02shell.sh
+++ 02shell.sh
@@ -2,7 +2,7 @@
 
 r=$1 s=$2
 
-echo "$1: Hello $2, how are you"
-echo "$1: I am fine $2, Goood morning"
-echo "$1: Thanks $2"
-echo "no problem $1"
+echo "$r: Hello $s, how are you"
+echo "$r: I am fine $s, Goood morning"
+echo "$r: Thanks $2"
+echo "no problem $s"
