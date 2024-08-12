0fce7fe881ba3da5dd18244ad58c8fa7f5026eea
unknown <pjuned.it@gmail.com>
2024-08-12 18:47:16 +0530

    second script

--- condition.sh
+++ condition.sh
@@ -2,9 +2,11 @@
 number=$1
 
 if [number -gt 100]
+    then
+    echo "givem number is greater than 100"
 
-echo "givem number is greater than 100"
+    else 
 
-else 
+    echo "it is less than 100"
 
-echo "it is less than 100"
+fi
