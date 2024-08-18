bc69a9f68d6d19f95a8ab055bad1b42634530775
unknown <pjuned.it@gmail.com>
2024-08-19 02:23:49 +0530

    edited

--- disk_usage.sh
+++ disk_usage.sh
@@ -9,11 +9,14 @@ do
 
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
-    if [$usage -ge $DISK_THRESHOLD ]
+    # if [ $usage -ge $DISK_THRESHOLD ]
+    # then
+    #     message+="High usage on $partition : $usage <br>"
+    # fi
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
-        message+="High usage on $partition : $usage"
+        message+="High Disk Usage on $partition: $usage <br>"
     fi
-
-done <<<$DISK_USAGE
+done <<< $DISK_USAGE
 
 echo "message=$message"
