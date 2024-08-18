6f1fa09396db972bddcae2abc7bb772de48cb73b
unknown <pjuned.it@gmail.com>
2024-08-19 01:00:06 +0530

    edited

---
+++ ifs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+file=/etc/passwd
+
+R="\e[31m"
+G="\e[32m"
+B="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file ]
+then
+    echo -e "$R Source dir $file does not exist $N"
+fi
+
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
+do 
+    echo "username : $username"
+    echo "userid : $userid"
+
+done < $file
